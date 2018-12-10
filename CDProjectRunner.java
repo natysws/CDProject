536ad99f8b402a82dd5b50ae2ef5b9d8446513e5
yungharvard <45724800+yungharvard@users.noreply.github.com>
2018-12-09 22:24:50 -0500

    Update CDProjectRunner.java

--- CDProjectRunner.java
+++ CDProjectRunner.java
@@ -15,7 +15,9 @@ public class CDProjectRunner {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       Scanner file = new Scanner(new File("CD_Dade.txt"));
+       String CD = file.nextLine()
+       
     }
     
 }
